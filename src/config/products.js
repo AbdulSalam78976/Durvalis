dc8ad2c6b6302815690a47c4b46cefa9713bac0a
AbdulSalam78976 <mrdj78976@gmail.com>
2026-01-19 18:33:54 +0500

    price changed

--- src/config/products.js
+++ src/config/products.js
@@ -35,7 +35,7 @@ export const products = {
     id: 'pack-1',
     name: 'Single Tube',
     quantity: 1,
-    price: 0.1,
+    price: 0.5,
     originalPrice: 19.99,
     savings: 5.00,
     popular: false,
